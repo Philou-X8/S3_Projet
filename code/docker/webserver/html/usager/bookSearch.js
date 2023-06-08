54c2bbcbd7aa31ca21c92fae27dd277f4ae6d27c
Philou-X8 <pdurettesds@gmail.com>
2023-06-08 11:32:54 -0400

    More book info

--- code/docker/webserver/html/usager/bookSearch.js
+++ code/docker/webserver/html/usager/bookSearch.js
@@ -1,6 +1,20 @@
 
 
-
+function printBookInfo(book){
+    const span = document.getElementById('title').firstElementChild;
+    var urlLink = book.data.url.toString();
+    span.innerHTML = '<br> <strong>' + 'Search complete' + '</strong> </br>' +
+        '<br>Titre: ' + book.data.label + '</br>' +
+        '<br>Cours: ' + book.data.sigle + '</br>' +
+        '<br>Auteur: ' + book.data.author_id + ', Editeur: ' + book.data.editor_id + '</br>' +
+        '<br>ISBN: ' + book.data.codeisbn + '</br>' +
+        //'<br>URL: ' + urlLink.link(book.data.url) + '</br>' +
+        '<br>URL: ' + '<a id="book_link" target="_blank">' + urlLink + '</a></br>' +
+        '<br>Publication: ' + book.data.publicationdate +
+        ', Langue: ' + book.data.language_id +
+        ', Format: ' + book.data.format_id + '</br>'
+    document.getElementById("book_link").setAttribute("href",urlLink);
+}
 function requestFromID() {
     const div = document.getElementById('title');
     var idBook = document.getElementById('book_id_in');
@@ -13,8 +27,9 @@ function requestFromID() {
     })
         .then(function (response) {
             console.log("Response: ", response.status);
-            span.innerHTML = '<br> <strong>' + 'button clicked' + '</strong> </br>' +
-                '<br> <strong>' + response.data.codeisbn + '</strong> </br>'
+            printBookInfo(response);
+            //span.innerHTML = '<br> <strong>' + 'button clicked' + '</strong> </br>' +
+            //    '<br> <strong>' + response.data.codeisbn + '</strong> </br>'
         })
         .catch(function (error) {
             console.log('refreshing');
@@ -41,8 +56,7 @@ function requestFromSigle() {
     })
         .then(function (response) {
             console.log("Response: ", response.status);
-            span.innerHTML = '<br> <strong>' + 'button clicked' + '</strong> </br>' +
-                '<br> <strong>' + response.data.codeisbn + '</strong> </br>'
+            printBookInfo(response);
         })
         .catch(function (error) {
             console.log('refreshing');
